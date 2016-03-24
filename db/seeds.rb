7cfef1a8859fd569cf196a8eeaa92d5ebd3d3942
szymonlipka <revan12311@gmail.com>
2016-03-24 14:33:19 +0100

    Seeds add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+id_char = 'a'
+10.times {Job.create(name: 'Job', id_char: id_char.next!)}
