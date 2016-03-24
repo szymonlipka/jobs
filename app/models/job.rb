2ee9f412fe61295221fd9e5097647d4387df779f
szymonlipka <revan12311@gmail.com>
2016-03-24 15:32:16 +0100

    Order method small refactor and change of old hashing syntax in flash messages

--- app/models/job.rb
+++ app/models/job.rb
@@ -8,22 +8,26 @@ class Job < ActiveRecord::Base
     jobs = Job.where(id_char: string.split(''))
     number_of_circulments = 0
     ##
-    # since circuling in this problem is possible the loop stops when it reaches max number of
-    # circulments and raises error
+    # since circling without end in this problem is possible the loop stops when it reaches max number of
+    # circles and raises an error
     while swapped && number_of_circulments < string.size ** 2 + 1 do 
       swapped = false
       i = 0
       while i < string.size - 1
         if job = jobs.detect {|f| f[:id_char] == string[i]}
-          if job.more_important_jobs && job.more_important_jobs.include?(string[i])
-            raise ArgumentError
-          elsif job.more_important_jobs
-            chars_to_move = (string[i..string.size-1].chars & job.more_important_jobs.chars).join
-            if !chars_to_move.empty?
-              string.delete!(chars_to_move)
-              string.insert(i, chars_to_move)
-              i += chars_to_move.size
-              swapped = true
+          if job.more_important_jobs
+            if job.more_important_jobs.include?(string[i])
+              raise ArgumentError
+            else
+              # next line takes common part of everything thats after current character and characters
+              # which are more important then current
+              chars_to_move = (string[i..string.size-1].chars & job.more_important_jobs.chars).join
+              if !chars_to_move.empty?
+                string.delete!(chars_to_move)
+                string.insert(i, chars_to_move)
+                i += chars_to_move.size
+                swapped = true
+              end
             end
           end
         else
@@ -33,7 +37,7 @@ class Job < ActiveRecord::Base
       end
       number_of_circulments += 1
     end
-    if swapped # loop ended because of reaching max number of circulments and swap still was made then it raises error
+    if swapped # loop ended because of reaching max number of circles and swap still was made then it raises error
       raise 'Dependencies circuled'
     else
       string
