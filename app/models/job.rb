cefb600b12792d412c21efee65d5b96f87c594ae
szymonlipka <revan12311@gmail.com>
2016-03-24 14:39:09 +0100

    A bit more seeds added and small refactor of order method

--- app/models/job.rb
+++ app/models/job.rb
@@ -14,17 +14,20 @@ class Job < ActiveRecord::Base
       swapped = false
       i = 0
       while i < string.size - 1
-        job = jobs.detect {|f| f[:id_char] == string[i]}
-        if job.more_important_jobs && job.more_important_jobs.include?(string[i])
-          raise ArgumentError
-        elsif job.more_important_jobs
-          chars_to_move = (string[i..string.size-1].chars & job.more_important_jobs.chars).join
-          if !chars_to_move.empty?
-            string.delete!(chars_to_move)
-            string.insert(i, chars_to_move)
-            i += chars_to_move.size
-            swapped = true
+        if job = jobs.detect {|f| f[:id_char] == string[i]}
+          if job.more_important_jobs && job.more_important_jobs.include?(string[i])
+            raise ArgumentError
+          elsif job.more_important_jobs
+            chars_to_move = (string[i..string.size-1].chars & job.more_important_jobs.chars).join
+            if !chars_to_move.empty?
+              string.delete!(chars_to_move)
+              string.insert(i, chars_to_move)
+              i += chars_to_move.size
+              swapped = true
+            end
           end
+        else
+          string.delete_at(i)
         end
         i+=1
       end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 id_char = 'a'
 10.times {Job.create(name: 'Job', id_char: id_char.next!)}
+Job.find_by(id_char: 'd').update_attribute(:more_important_jobs, 'h')
+Job.find_by(id_char: 'i').update_attribute(:more_important_jobs, 'dh')
+Job.find_by(id_char: 'f').update_attribute(:more_important_jobs, 'idh')
+Job.find_by(id_char: 'g').update_attribute(:more_important_jobs, 'fidh')
+Job.find_by(id_char: 'e').update_attribute(:more_important_jobs, 'gfidh')
+Job.find_by(id_char: 'b').update_attribute(:more_important_jobs, 'hdifge')
